0e03924d227478a34c4d75c68143eec91f265c69
HiddenRambler <rambler@hiddenramblings.com>
2014-01-21 18:16:41 +0000

    Updated api and applied apache 2 license.

--- jni/aihl.c
+++ jni/aihl.c
@@ -1,30 +1,20 @@
 /*
- * Copyright (C) 2014 rambler@hiddenramblings.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+
+Copyright (C) 2014 rambler@hiddenramblings.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
  
 #include <dlfcn.h>
 #include <errno.h>
@@ -132,16 +122,26 @@ static Elf_Sym* lookup_symbol(soinfo *si, const char *symbolname) {
     return NULL;
 }
 
-void *aihl_hook_symbol(const char *libname, const char *symbolname, void *hookfunc) {
-    soinfo *si = (soinfo *)dlopen(libname, RTLD_GLOBAL);
-    if (!si) {
+
+void *aihl_load_library(const char *libname) {
+	void *handle = dlopen(libname, RTLD_GLOBAL);
+	if (!handle) {
         log("Failed to load lib %s\n", libname);
+	}
+	
+	return handle;
+}
+
+void *aihl_hook_symbol(void * libhandle, const char *symbolname, void *hookfunc) {
+    soinfo *si = (soinfo *)libhandle;
+    if (!si) {
+        log("Invalid handle\n");
         return NULL;
     }
 
     Elf_Sym* symbol = lookup_symbol(si, symbolname);
     if (!symbol) {
-        log("Failed to find symbol %s in lib %s\n", symbolname, libname);
+        log("Failed to find symbol %s\n", symbolname);
         return NULL;
     }
     Elf32_Word originalfunc = si->load_bias + symbol->st_value;
